de4c06b3c1b1d71f8455435a361accda99ea22a8
sibeu <Seugeny97@gmail.com>
2023-03-03 12:14:58 +0300

    added storybook, loki

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,5 +1,5 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { buildCssLoader } from './loaders/buildCssLoader';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
@@ -10,24 +10,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		use: ['@svgr/webpack'],
 	}
 
-	const cssLoader = {
-		test: /\.s[ac]ss$/i,
-		use: [
-			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-			{
-				loader: 'css-loader',
-				options: {
-					modules: {
-						auto: /\.module\.\w+$/,
-						localIdentName: isDev 
-							? '[path][name]__[local]--[hash:base64:5]' 
-							: '[hash:base64:8]',
-					}
-				},
-			},
-			'sass-loader',
-		],
-	}
+	const cssLoader = buildCssLoader(isDev);
 
 	const typescriptLoader = {
 		test: /\.tsx?$/,
--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -4,27 +4,28 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './AppLink.module.scss';
 
 export enum AppLinkTheme {
-    PRIMARY = 'primary',
-    SECONDARY = 'secondary',
+	PRIMARY = 'primary',
+	SECONDARY = 'secondary',
+	RED = 'red',
 }
 
 interface AppLinkProps extends LinkProps {
-    className?: string;
-    theme?: AppLinkTheme;
+	className?: string;
+	theme?: AppLinkTheme;
 }
 
 const AppLink: FC<AppLinkProps> = (props) => {
 	const {
 		to,
-		className, 
+		className,
 		children,
 		theme = AppLinkTheme.PRIMARY,
 		...otherProps
 	} = props;
 
 	return (
-		<Link 
-			to={to} 
+		<Link
+			to={to}
 			className={classNames(cls.AppLink, {}, [className, cls[theme]])}
 			{...otherProps}
 		>

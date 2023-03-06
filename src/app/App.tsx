7bae3e51356a8644aefdafd8337d43ddef6ac40a
sibeu <Seugeny97@gmail.com>
2023-03-06 11:29:51 +0300

    add modal, portal, change header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
-import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense, useEffect } from 'react';
+import { Suspense, useState } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
 import { AppRouter } from './providers/router';
 
 const App = () => {
 	const { theme } = useTheme();
+
+	const [isOpen, setIsOpen] = useState(false);
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,36 @@
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import Button, { ButtonTheme } from 'shared/ui/Button/Button';
+import Modal from 'shared/ui/Modal/Modal';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
 	className?: string;
 }
 
-const Navbar = ({ className }: NavbarProps) => (
-	<div className={classNames(cls.Navbar, {}, [className])}>
-		<div className={cls.links}>
-			/
+const Navbar = ({ className }: NavbarProps) => {
+	const { t } = useTranslation();
+	const [isAuthModal, setIsAuthModal] = useState(false);
+
+	const onToggleModal = useCallback(() => {
+		setIsAuthModal((prev) => !prev);
+	}, []);
+
+	return (
+		<div className={classNames(cls.Navbar, {}, [className])}>
+			<Button
+				theme={ButtonTheme.CLEAR_INVERTED}
+				className={cls.links}
+				onClick={onToggleModal}
+			>
+				{t('Войти')}
+			</Button>
+			<Modal isOpen={isAuthModal} onClose={onToggleModal}>
+				123
+			</Modal>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Navbar;

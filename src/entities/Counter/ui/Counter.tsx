aac337b605ea604c1beabcb3c6479fc48f3538b7
sibeu <Seugeny97@gmail.com>
2023-03-06 14:58:15 +0300

    fixed counterSlice error

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -2,7 +2,7 @@ import Button from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
-import { counterActions } from '../model/slice/CounterSlice';
+import { counterActions } from '../model/slice/counterSlice';
 
 export const Counter = () => {
 	const dispatch = useDispatch();
